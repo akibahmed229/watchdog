b30b51fb71cc4ed9462c8aa6efa71fcbfda6eeca
akibahmed229 <akib4418@gmail.com>
2024-12-07 20:33:09 +0600

    feat: scripts added

---
+++ scripts/nfs.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+server_path=$1
+server_ip_address=$2
+
+client_path=$3
+client_ip_address=$4
+
+# check if command line arguments are provided 
+if [ $# -le 3 ]; then
+    echo "Please provide the server path, server IP address, client path, and client IP address"
+    echo "--------------------------------------"
+    echo "--------------------------------------"
+    echo "Usage: nfs.sh <server_path> <server_ip_address> <client_path> <client_ip_address>"
+    exit 1
+fi
+
+function server_configuration() {
+    # Install NFS server
+    sudo apt install nfs-utils libnfsidmap
+
+    # enable and start the nfs-server service 
+    sudo systemctl enable nfs-server rpcbind 
+    sudo systemctl start nfs-server rpcbind rpc-statd nfs-idmap
+
+    # Add the port to the firewall 
+    sudo ufw allow nfs 
+    sudo ufw allow mountd
+    sudo ufw allow rpc-bind 
+
+    # Create a shared directory & setup the permissions
+    sudo mkdir -p $server_path
+    sudo chown nobody:nogroup $server_path
+    sudo chmod 777 $server_path
+
+    # Export the shared directory 
+    sudo echo "$server_path $client_ip_address(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+    exportfs -rv
+}
+
+function client_configuration(){
+    # Install NFS client
+    sudo apt install nfs-utils rpcbind
+
+    # Enable and start the nfs-client service 
+    sudo systemctl enable rpcbind
+    sudo systemctl start rpcbind
+
+    # Add the port to the firewall 
+    sudo ufw allow nfs 
+    sudo ufw allow mountd 
+    sudo ufw allow rpc-bind 
+    
+    # Create a directory to mount the shared directory 
+    sudo mkdir -p $client_path
+
+    # Mount the shared directory 
+    sudo mount $server_ip_address:$server_path $client_path
+}
+
+read -rp "Configure the server or client
+1. Server
+2. Client
+
+0. Exit
+" choice
+
+
+case $choice in
+    1)
+        server_configuration
+        ;;
+    2)
+        client_configuration
+        ;;
+    0)
+        exit 0
+        ;;
+    *)
+        echo "Invalid choice"
+        ;;
+esac
---
+++ scripts/postfix.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+
+mail=$1
+password=$2
+
+# check if the user has provided the email and password 
+if [ -z "$mail" ] || [ -z "$password" ] && [ $# -le 2 ]; then
+    echo "Please provide your email and password"
+    echo "--------------------------------------"
+    echo "--------------------------------------"
+    echo "Usage: postfix.sh <email> <password>"
+    exit 1
+fi
+
+# Install Postfix and mailutils 
+sudo apt-get install -y postfix mailutils neovim
+
+
+function setup_postfix() {
+    # Set up Postfix to use Gmail as a relay host 
+    sudo postconf -e 'relayhost = [smtp.gmail.com]:587'
+    sudo postconf -e 'myhostname= $(hostname -f)'
+    
+    # Location of sasl_passwd we saved
+    sudo postconf -e "smtp_sasl_password_maps = hash:/etc/postfix/sasl/sasl_passwd"
+    
+    # Enables SASL authentication for postfix
+    sudo postconf -e 'smtp_sasl_auth_enable = yes'
+    sudo postconf -e 'smtp_tls_security_level = encrypt'
+    
+    # Disallow methods that allow anonymous authentication
+    sudo postconf -e 'smtp_sasl_security_options = noanonymous'
+}
+
+function setup_postfix_passwdFile() {
+    # Create the sasl_passwd file
+    sudo touch /etc/postfix/sasl/sasl_passwd
+    
+    # Add Gmail credentials to the sasl_passwd file 
+    sudo echo "[smtp.gmail.com]:587 $mail:$password" > /etc/postfix/sasl/sasl_passwd
+    
+    # Change the permissions of the sasl_passwd file
+    sudo chmod 600 /etc/postfix/sasl/sasl_passwd
+    sudo chown root:root /etc/postfix/sasl/sasl_passwd
+    
+    # Update Postfix to use the sasl_passwd file and delete the plaintext file
+    sudo postmap /etc/postfix/sasl/sasl_passwd 
+    sudo rm /etc/postfix/sasl/sasl_passwd
+}
+
+
+# Set up Postfix 
+setup_postfix 
+setup_postfix_passwdFile
+
+# Restart postfix
+sudo systemctl restart postfix
